0b57cfe29fa59335becb238be3561ee20ecad591
ElitX10 <thomas.lescolan@sfr.fr>
2018-01-16 12:07:56 +0100

    tuto 1 fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   get 'say_hello/say'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
